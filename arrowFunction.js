e67e915d111a54e5d8a4a045f34e68ebd6a4a4bc
zenyhoj <joe.balingit@gmail.com>
2018-10-18 16:22:30 +0800

    more => example

--- arrowFunction.js
+++ arrowFunction.js
@@ -10,9 +10,23 @@ console.log(sum(2, 3));
 const person = {
     name: 'joe',
     lastName: 'balingit',
-    getFullName: function(name, lastName) { 
+    getFullName: function() { 
         console.log(`${this.name} ${this.lastName}`);
     } //using => will point to global 
      
 }
 console.log(person.getFullName());
+
+// 
+let team = {
+    members: ['jane', 'joe'], 
+    teamName: 'Super coders',
+    getTeamName: function() {
+        return this.members.map((member)=> { //=> this.teamName will point to the current object
+return `${member} is on team ${this.teamName}`;
+        });
+}
+}
+
+console.log(team.getTeamName());
+
